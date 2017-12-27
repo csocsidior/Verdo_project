0931191f392be1d62cd360236d2270ff59c7fbc0
Krisztian Balla <cballa7@gmail.com>
2017-12-27 02:09:58 +0100

    soft up

--- Software/Identification_EPANET/Modified EPANET model/Data/Data_identification.m
+++ Software/Identification_EPANET/Modified EPANET model/Data/Data_identification.m
@@ -1,6 +1,7 @@
-t=0:1:144; % Time vector
-
-%% 
+%%
+clear all;
+t=0:1:120; % Time vector
+ 
 %//////////Identification//////////
 load('dk_1.mat');
 load('dk_2.mat');
@@ -9,8 +10,7 @@ load('p_B.mat');
 load('p_C.mat');
 load('pk_1.mat');
 load('pk_2.mat');
-dk_2 = dk_2*-1;
-
+dk_2 = dk_2*-1; 
 
 %for calculating sigma:
 load('HL7_demand.mat');   
@@ -21,6 +21,18 @@ load('HL11_demand.mat');
 load('HogL52_demand.mat'); 
 
 sigma = -HL10_demand - HL11_demand -HL7_demand - HogL52_demand + dk_1 + dk_2;
+
+%removing the first day calculation
+
+n=24;
+dk_1(1:n,:) = [];   
+dk_2(1:n,:) = []; 
+p_A(1:n,:) = [];
+p_B(1:n,:) = [];
+p_C(1:n,:) = [];
+pk_1(1:n,:) = [];
+pk_2(1:n,:) = [];
+sigma(1:n,:) = [];
 
 %%
 %Training set for the output identification
@@ -34,7 +46,7 @@ Y_K = [pk_1, pk_2]';
 
 spread = 145;                 %66 
 % number of neurons
-K = 10;                       %6
+K = 6;                       %6
 % performance goal 
 goal = 0.001;
 % neuron step
@@ -44,11 +56,15 @@ net = newrb(X_K,Y_K,goal,spread,K,Ki);
 Y_net = net(X_K);
 a{1} = radbas(netprod(dist(net.IW{1,1},X_K),net.b{1}));
 
- chi_K = [a{1} ; p_A' ; p_B'; p_C'; ones(1,145)];
+ chi_K = [a{1} ; p_A' ; p_B'; p_C'; ones(1,121)];
  theta_K = Y_K/chi_K
  Z_K = theta_K*chi_K;
  
  figure(10)
+stairs(t,Z_K(1,:),'LineWidth',1.2)
+ hold on
+ stairs(t,pk_1,'LineWidth',1.2)
+  figure(11)
 stairs(t,Z_K(2,:),'LineWidth',1.2)
  hold on
  stairs(t,pk_2,'LineWidth',1.2)
@@ -57,7 +73,7 @@ stairs(t,Z_K(2,:),'LineWidth',1.2)
 
 figure(1)
 stairs(t,sigma,'LineWidth',1.2)
-xlim([25 149])
+xlim([0 121])
 %  hold on
 %  stairs(t,dk_1 + dk_2,'LineWidth',1.2)
 %  hold on
@@ -67,24 +83,24 @@ figure(2)
 stairs(t,dk_1,'LineWidth',1.2)
 hold on
 stairs(t,dk_2,'LineWidth',1.2)
-xlim([25 149])
+xlim([0 121])
 
 figure(3)
 stairs(t,pk_1,'LineWidth',1.2)
-xlim([25 149])
+xlim([0 121])
 
 figure(4)
 stairs(t,pk_2,'LineWidth',1.2)
-xlim([25 149])
+xlim([0 121])
 
 figure(5)
 stairs(t,p_A,'LineWidth',1.2)
-xlim([25 149])
+xlim([0 121])
 
 figure(6)
 stairs(t,p_B,'LineWidth',1.2)
-xlim([25 149])
+xlim([0 121])
 
 figure(7)
 stairs(t,p_C,'LineWidth',1.2)
-xlim([25 149])
+xlim([0 121])
