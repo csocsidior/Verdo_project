1c7f75bc8bd4c61fc23fe03c9b94e58ac5ba041c
Krisztian Balla <cballa7@gmail.com>
2018-01-02 11:28:38 +0100

    New figure

--- Software/Identification_EPANET/Modified EPANET model/Data/Data_identification.m
+++ Software/Identification_EPANET/Modified EPANET model/Data/Data_identification.m
@@ -44,7 +44,7 @@ Y_K = [pk_1, pk_2]';
 
 %% Output identification
 
-spread = 145;                 %66 
+spread = 150;                 %145 
 % number of neurons
 K = 6;                       %6
 % performance goal 
@@ -68,39 +68,76 @@ stairs(t,Z_K(1,:),'LineWidth',1.2)
 stairs(t,Z_K(2,:),'LineWidth',1.2)
  hold on
  stairs(t,pk_2,'LineWidth',1.2)
+ 
+ cost_func = 'NRMSE';
+ fit_pk1 = 100*goodnessOfFit(Z_K(1,:)',pk_1,cost_func)
+ fit_pk2 = 100*goodnessOfFit(Z_K(2,:)',pk_2,cost_func)
+
 
 %% Figures
 
 figure(1)
 stairs(t,sigma,'LineWidth',1.2)
-xlim([0 121])
-%  hold on
-%  stairs(t,dk_1 + dk_2,'LineWidth',1.2)
-%  hold on
- % stairs(t,HL10_demand + HL11_demand,'LineWidth',1.2)
+xlim([0 120])
+ylim([15 108])
+xlabel('Time [h]','interpreter','latex');
+ ylabel('Flow  [LPS]','interpreter','latex')
+ title('Total water consumption - $\sigma$','Interpreter', 'latex');
+ 
+%% 
 
 figure(2)
-stairs(t,dk_1,'LineWidth',1.2)
-hold on
 stairs(t,dk_2,'LineWidth',1.2)
-xlim([0 121])
+hold on
+stairs(t,dk_1,'LineWidth',1.2)
+ylim([0 105])
+xlim([0 120])
+xlabel('Time [h]','interpreter','latex');
+ylabel('Flow  [LPS]','interpreter','latex')
+h1 = legend('$\bar{d}_{\mathcal{K}1}$','$\bar{d}_{\mathcal{K}2}$','Location','NorthEast');
+set(h1, 'Interpreter', 'latex');
+title('Inlet flows','Interpreter', 'latex');
 
+
+%% Don't export this
 figure(3)
 stairs(t,pk_1,'LineWidth',1.2)
 xlim([0 121])
 
+%% Don't export this
 figure(4)
 stairs(t,pk_2,'LineWidth',1.2)
 xlim([0 121])
 
+%%
+
 figure(5)
 stairs(t,p_A,'LineWidth',1.2)
-xlim([0 121])
+xlim([0 120])
+ylim([2.45 3.1])
+xlabel('Time [h]','interpreter','latex');
+ylabel('Head  [m]','interpreter','latex')
+h1 = legend('$\hat{p}_{\mathcal{W}1}$','Location','SouthEast');
+set(h1, 'Interpreter', 'latex');
 
+%%
 figure(6)
 stairs(t,p_B,'LineWidth',1.2)
-xlim([0 121])
+xlim([0 120])
+ylim([2.92 3.02])
+xlabel('Time [h]','interpreter','latex');
+ylabel('Head  [m]','interpreter','latex')
+h1 = legend('$\hat{p}_{\mathcal{W}2}$','Location','SouthEast');
+set(h1, 'Interpreter', 'latex');
+
+%%
 
 figure(7)
 stairs(t,p_C,'LineWidth',1.2)
-xlim([0 121])
+xlim([0 120])
+ylim([2.52 2.77])
+xlabel('Time [h]','interpreter','latex');
+ylabel('Head  [m]','interpreter','latex')
+h1 = legend('$\hat{p}_{\mathcal{W}2}$','Location','SouthEast');
+set(h1, 'Interpreter', 'latex');
+
